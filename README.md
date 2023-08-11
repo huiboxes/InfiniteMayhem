c106047ac6a2cbf4002a243af3cafc32596b92f6
kylo <huiboxes@gmail.com>
2023-08-11 16:30:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Infinite Mayhem
+
+使用 Unreal Engine 制作的（Cpp + 蓝图） FPS 游戏。求职用。
---
+++ Source/InfiniteMayhem/InfiniteMayhemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfiniteMayhemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITEMAYHEM_API AInfiniteMayhemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
