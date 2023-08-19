12ef93981d87f56e0f5eb07be9f739c84c010433
kylo <huiboxes@gmail.com>
2023-08-19 22:23:06 +0800

    feat: 添加屏幕中心点

--- Source/InfiniteMayhem/UI/IMFPSHUD.cpp
+++ Source/InfiniteMayhem/UI/IMFPSHUD.cpp
@@ -13,22 +13,28 @@ void AIMFPSHUD::DrawHUD() {
 }
 
 void AIMFPSHUD::DrawCrosshair() {
-	ASWATCharacter* Player = Cast<ASWATCharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
-	if (!Player || !Player->IsHoldWeapon()) return;
-
 	float TargetX = Canvas->ClipX / 2;
 	float TargetY = Canvas->ClipY / 2;
 
-	float LineLength = 10;
-	float Offset = 5;
+	// 绘制中心点
+	DrawRect(FLinearColor::Yellow, TargetX - 1, TargetY - 1, 2, 2);
 
-	// 上
-	DrawLine(TargetX, TargetY - (LineLength + Offset), TargetX, TargetY - Offset, FLinearColor::Yellow);
-	// 下
-	DrawLine(TargetX, TargetY + Offset, TargetX, TargetY + LineLength + Offset, FLinearColor::Yellow);
+	ASWATCharacter* Player = Cast<ASWATCharacter>(GetWorld()->GetFirstPlayerController()->GetCharacter());
+	if (Player && Player->IsHoldWeapon()) { // 如果玩家持有枪械时，绘制十字准星
+		float LineLength = 10;
+		float Offset = 5;
+
+		// 上
+		DrawLine(TargetX, TargetY - (LineLength + Offset), TargetX, TargetY - Offset, FLinearColor::Yellow);
+		// 下
+		DrawLine(TargetX, TargetY + Offset, TargetX, TargetY + LineLength + Offset, FLinearColor::Yellow);
+
+		// 右
+		DrawLine(TargetX + Offset, TargetY, TargetX + LineLength + Offset, TargetY, FLinearColor::Yellow);
+		// 左
+		DrawLine(TargetX - (LineLength + Offset), TargetY, TargetX - Offset, TargetY, FLinearColor::Yellow);
+	}
+
 	
-	// 右
-	DrawLine(TargetX + Offset, TargetY, TargetX + LineLength + Offset, TargetY,FLinearColor::Yellow);
-	// 左
-	DrawLine(TargetX - (LineLength + Offset), TargetY, TargetX - Offset, TargetY, FLinearColor::Yellow);
 }
+
