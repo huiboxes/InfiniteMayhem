86dcd0fd481f70168638648b0f5866724fef00b7
kylo <huiboxes@gmail.com>
2023-09-05 12:22:22 +0800

    fix: 调整 Scar 资产

---
+++ Source/InfiniteMayhem/Zombie/ZombieCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ZombieCharacter.h"
+
+// Sets default values
+AZombieCharacter::AZombieCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AZombieCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombieCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AZombieCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
